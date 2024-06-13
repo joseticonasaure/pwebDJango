cbf15f9f28222715529f9445cec1078371fb7cf1
jose ticona saure <josetico876@gmail.com>
2024-06-13 06:12:27 -0500

    Definir modelo Persona en la aplicación personas

--- Django/personas/models.py
+++ Django/personas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    email = models.EmailField(unique=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    telefono = models.CharField(max_length=15, null=True, blank=True)
